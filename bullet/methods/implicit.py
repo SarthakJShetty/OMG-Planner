gradient descent on end effector pose without CHOMP to a fixed goal

bullet/methods/implicit.py
@@ -21,11 +21,11 @@ class ImplicitGrasp_NoVision:
         outpose = outpose.squeeze(0)
         return outpose
 
-class ImplicitGrasp_OutputPose:
+class ImplicitGrasp_OutputPose: # Also valid for OutputDist
     def __init__(self, ckpt_path):
         # ckpt = torch.load('/checkpoint/thomasweng/grasp_manifolds/runs/outputs/nopc_book_logmap_10kfree/2021-11-23_232102_dsetacronym_Book_train0.9_val0.1_free10.0k_sym_distlogmap/default_default/0_0/checkpoints/last.ckpt')
         ckpt = torch.load(ckpt_path)
-        self.model = Decoder(**ckpt['hyper_parameters'])
+        self.model = Decoder(**ckpt['hyper_parameters'], intype='pq').double() # TODO remove intype for non pose 
         self.model.cuda()
         self.model.load_state_dict(ckpt['state_dict'])
         self.model.eval()
@@ -34,8 +34,27 @@ class ImplicitGrasp_OutputPose:
         """
         x: xyz, xyzw
         """
-        with torch.no_grad():
-            outpose = self.model(x)
+        # with torch.no_grad():
+        outpose = self.model(x)
         # outpose = outpose.squeeze(0).cpu().numpy()
         outpose = outpose.squeeze(0)
         return outpose
+
+# class ImplicitGrasp_OutputDist:
+#     def __init__(self, ckpt_path):
+#         # ckpt = torch.load('/checkpoint/thomasweng/grasp_manifolds/runs/outputs/nopc_book_logmap_10kfree/2021-11-23_232102_dsetacronym_Book_train0.9_val0.1_free10.0k_sym_distlogmap/default_default/0_0/checkpoints/last.ckpt')
+#         ckpt = torch.load(ckpt_path)
+#         self.model = Decoder(**ckpt['hyper_parameters'])
+#         self.model.cuda()
+#         self.model.load_state_dict(ckpt['state_dict'])
+#         self.model.eval()
+
+#     def forward(self, x):
+#         """
+#         x: xyz, xyzw
+#         """
+#         with torch.no_grad():
+#             outpose = self.model(x)
+#         # outpose = outpose.squeeze(0).cpu().numpy()
+#         outpose = outpose.squeeze(0)
+#         return outpose

omg/cost.py
@@ -480,7 +480,7 @@ class Cost(object):
             weighted_goal_grad = np.zeros_like(weighted_obs_grad)
             weighted_goal_grad[-1, :7] = self.cfg.grasp_weight * traj.goal_grad
             # weighted_goal_grad[-1, :7] = 1.0 * traj.goal_grad
-            print(f"w_obs_cost: {weighted_obs}, w_smooth: {weighted_smooth}, w_goal: {weighted_goal_cost}")
+            print(f"obs wt: {self.cfg.obstacle_weight}, smooth wt: {self.cfg.smoothness_weight}, goal wt: {self.cfg.grasp_weight}")
             cost = weighted_obs + weighted_smooth + weighted_goal_cost
             grad = weighted_obs_grad + weighted_smooth_grad + weighted_goal_grad
 